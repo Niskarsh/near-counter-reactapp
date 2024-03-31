0472e82912eb58c63f13c414da3940cc0f971bcf
Niskarsh <niskarshk@gmail.com>
2024-03-31 13:26:57 +0530

    Fixed some issues, and set up wallet integration

--- src/App.js
+++ src/App.js
@@ -3,28 +3,40 @@ import { setupWalletSelector } from "@near-wallet-selector/core";
 import { setupModal } from "@near-wallet-selector/modal-ui";
 import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
 import './App.css';
+import "@near-wallet-selector/modal-ui/styles.css"
+window.Buffer = window.Buffer || require("buffer").Buffer;
 
 class App extends Component {
   state = {
     counter: 0,
     CONTRACT_ADDRESS: 'niskarsh31.testnet',
-    NETWORK: 'testnet'
+    NETWORK: 'testnet',
+    walletModalVisible: false,
+    walletModal: {},
   };
 
+  toggleWalletModalVisbibility = () => {
+    let visible = this.state.walletModalVisible;
+    this.setState({ walletModalVisible: !visible});
+  }
+
   async componentDidMount () {
+    console.log("called")
     const selector = await setupWalletSelector({
       network: "testnet",
       modules: [setupMeteorWallet()],
     });
+    console.log(selector.isSignedIn());
     const modal = setupModal(selector, {
       contractId: "test.testnet",
     });
+    this.setState({ walletModal: modal });
     
-    modal.show();
+    // modal.show(this.state.walletModalVisible);
   }
 
   render() {
-    let { counter } = this.state;
+    let { counter, walletModal, walletModalVisible } = this.state;
   return (
     <div className="App">
       <h1>This counter lives in the NEAR blockchain!</h1>
@@ -33,6 +45,10 @@ class App extends Component {
       <br/>
       <br/>
       <button> Decrease </button>
+      <br/>
+      <br/>
+      <button onClick={this.toggleWalletModalVisbibility}>Connect wallet</button>
+      <div>{walletModal.show? walletModal.show(walletModalVisible): null}</div>
     </div>
   )
 }
