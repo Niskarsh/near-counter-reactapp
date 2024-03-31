99ec2c6828c9535c0dcae0a0987d810e4128209a
Niskarsh <niskarshk@gmail.com>
2024-03-31 12:15:02 +0530

    Changes before eject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { setupWalletSelector } from "@near-wallet-selector/core";
+import { setupModal } from "@near-wallet-selector/modal-ui";
+import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
 import './App.css';
 
-function App() {
+class App extends Component {
+  state = {
+    counter: 0,
+    CONTRACT_ADDRESS: 'niskarsh31.testnet',
+    NETWORK: 'testnet'
+  };
+
+  async componentDidMount () {
+    const selector = await setupWalletSelector({
+      network: "testnet",
+      modules: [setupMeteorWallet()],
+    });
+    const modal = setupModal(selector, {
+      contractId: "test.testnet",
+    });
+    
+    modal.show();
+  }
+
+  render() {
+    let { counter } = this.state;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This counter lives in the NEAR blockchain!</h1>
+      <h2>Counter: {counter}</h2>
+      <button> Increase </button>
+      <br/>
+      <br/>
+      <button> Decrease </button>
     </div>
-  );
+  )
+}
 }
 
 export default App;
