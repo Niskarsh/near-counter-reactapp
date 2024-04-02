a9b1c4236488ef249a56ef283c226da627ae70b9
Niskarsh <niskarshk@gmail.com>
2024-04-02 13:25:28 +0530

    Finised main code

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ class App extends Component {
     NETWORK: 'testnet',
     wallet: {},
     walletSignedIn: false,
-    caller: 'Sign in to access counT'
+    caller: 'Sign in to access counT',
+    increment: 'Increase',
+    decrement: 'Decrease',
+    reset: 'Reset'
   };
 
   toggleWalletModalVisbibility = () => {
@@ -48,6 +51,34 @@ class App extends Component {
     this.setState({ wallet: {} });
   }
 
+  increase = async () => {
+    const { wallet, CONTRACT_ADDRESS } = this.state;
+    this.setState({ increment: 'Pending' })
+    await wallet.callMethod({ contractId: CONTRACT_ADDRESS, method: 'increment' });
+    let newValue = await this.currentValue({ wallet, CONTRACT_ADDRESS });
+    this.setState({ counter: newValue, increment: 'Increase' });
+  }
+
+  decrease = async () => {
+    const { wallet, CONTRACT_ADDRESS } = this.state;
+    this.setState({ decrement: 'Pending' })
+    await wallet.callMethod({ contractId: CONTRACT_ADDRESS, method: 'decrement' });
+    let newValue = await this.currentValue({ wallet, CONTRACT_ADDRESS });
+    this.setState({ counter: newValue, decrement: 'Decrease' });
+  }
+
+  reset = async  () => {
+    const { wallet, CONTRACT_ADDRESS } = this.state;
+    this.setState({ reset: 'Pending' })
+    await wallet.callMethod({ contractId: CONTRACT_ADDRESS, method: 'reset' });
+    let newValue = await this.currentValue({ wallet, CONTRACT_ADDRESS });
+    this.setState({ counter: newValue, reset: 'Reset' });
+  }
+
+  currentValue = async ({ wallet, CONTRACT_ADDRESS }) => wallet.viewMethod({
+    contractId: CONTRACT_ADDRESS, method: 'get_num',
+  });
+
   async componentDidMount() {
     let { wallet, CONTRACT_ADDRESS, NETWORK } = this.state;
     if (!(Object.keys(wallet).length)) {
@@ -55,26 +86,30 @@ class App extends Component {
         createAccessKeyFor: CONTRACT_ADDRESS,
         network: NETWORK,
       });
-    } 
+    }
     let isSignedIn = await wallet.startUp();
-    this.setState({ wallet, walletSignedIn: isSignedIn,
+    let counter = await this.currentValue({ wallet, CONTRACT_ADDRESS }); 
+    this.setState({ wallet, walletSignedIn: Boolean(isSignedIn), counter,
       caller: isSignedIn ? `Welcome: ${wallet.accountId}`: 'Sign in to access counT'
     });
   }
 
   render() {
-    let { counter, walletSignedIn, caller } = this.state;
+    let { counter, walletSignedIn, caller, increment, decrement, reset } = this.state;
     return (
       <div className="App">
         <h1>This counter lives in the NEAR blockchain! [TESTNET]</h1>
+        <p>Share with your friends, or just watch the counter go</p>
+        <p>To participate, login</p>
+        <p>Once done, reset the counter. OR leave it for some other time</p>
         <h2>{ caller }</h2>
         
         <button onClick={this.walletSignIn} hidden={walletSignedIn} >Connect wallet</button>
         <button onClick={this.walletSignOut} hidden={!walletSignedIn}>Disconnect wallet</button>
         <h2>Counter: {counter}</h2>
-        <button> Increase </button>
-        <button> Decrease </button>
-        {/* <div>{walletModal.show? walletModal.show(walletModalVisible): null}</div> */}
+        <button onClick={this.increase} disabled={!walletSignedIn} l> {increment} </button>
+        <button onClick={this.decrease} disabled={!walletSignedIn}> {decrement} </button>
+        <button onClick={this.reset} disabled={!walletSignedIn}> {reset} </button>
       </div>
     )
   }
